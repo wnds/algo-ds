7ea8dc40e7f53446041cea6ed3bf807d029b61fe
mail2vks <singh.kr.vivek@gmail.com>
2014-04-29 21:51:35 +0530

    Adding name change

--- algorithms/sorting/test/test_insertionSort.py
+++ algorithms/sorting/test/test_insertionSort.py
@@ -2,13 +2,22 @@ import unittest
 from insertionSort import insertionSort
 from generateInput import SeedData
 import os.path
+import sys
 
 class insertionSortTest(unittest.TestCase):
 
     def setUp(self):
         sd = SeedData()
-        self.fileName = sd.genrateRandomDataTill(23)
+        self.fileName = sd.genrateRandomDataTill(10)
 
     def testFileExists(self):
-        self.assertTrue(os.path.isfile(self.fileName));
+        self.assertTrue(os.path.isfile(self.fileName))
         print "Checked that file %s exists" % self.fileName
+
+    def testInsertionSortWith10Numbers(self):
+    	sys.argv = ["", self.fileName]
+    	inst = insertionSort.insertionSort()
+    	inst.setUpSort()
+
+    def tearDown(self):
+    	os.remove(self.fileName)
